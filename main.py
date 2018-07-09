9e6fba793684744aabf1279c590cef03d71cf412
jetsalinas <joseenrico.salinas@gmail.com>
2018-07-09 17:49:43 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+RESTFUL api for Hackatren Bantay Sakay app
+:Author:     Jose Salinas
+:Author:     Maded Batara III
+:Version:    v20180709
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+
+
+#######
+# APP #
+#######
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
