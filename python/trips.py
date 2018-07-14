db2b601dcc5724c9b4565f1983c94f1510bd99fe
jetsalinas <joseenrico.salinas@gmail.com>
2018-07-14 23:04:58 +0800

    Added museum runs

--- python/trips.py
+++ python/trips.py
@@ -28,7 +28,16 @@ class Trips:
 
     def __init__(self, attractions):
         self.trips = [
-            Trip("Museum Run", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0])
+            Trip("Museum Run", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0]),
+            TripNode(1, attractions[1])
+
+            ]),
+            Trip("Museum Run 2", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0]),
+            TripNode(1, attractions[1])
+
+            ]),
+            Trip("Museum Run 3", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0]),
+            TripNode(1, attractions[1])
 
             ])
         ]
