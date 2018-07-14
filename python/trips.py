5e4072c6c9084ecf16a1ef293a39706c75011747
jetsalinas <joseenrico.salinas@gmail.com>
2018-07-15 06:13:38 +0800

    Added trips

--- python/trips.py
+++ python/trips.py
@@ -12,15 +12,17 @@ class TripNode:
 
 class Trip:
 
-    def __init__(self, name, description, tripNodes):
+    def __init__(self, name, description, tripNodes, imageUrl):
         self.name = name
         self.description = description
         self.tripNodes = tripNodes
+        self.imageUrl = imageUrl
 
     def toDict(self):
         return {
             "name": self.name,
             "description": self.description,
+            "imageUrl": self.imageUrl,
             "nodes": [tripNode.toDict() for tripNode in self.tripNodes]
             }
 
@@ -31,15 +33,8 @@ class Trips:
             Trip("Museum Run", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0]),
             TripNode(1, attractions[1]),
             TripNode(2, attractions[2])
-            ]),
-            Trip("Museum Run 2", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0]),
-            TripNode(1, attractions[1]),
-            TripNode(2, attractions[2])
-            ]),
-            Trip("Museum Run 3", "Discover a series of Manila's finest museums", [TripNode(0, attractions[0]),
-            TripNode(1, attractions[1]),
-            TripNode(2, attractions[2])
-            ])
+            ], "http://www.mcadmanila.org.ph/wp-content/uploads/2016/11/MCAD-SDA-1024x630-e1479456928953.jpg"),
+            Trip("Bargain and Bites", "Find the cheapest and most delicious eats in the city.", [], "https://www.kawalingpinoy.com/wp-content/uploads/2013/07/tokneneng2680.jpg")
         ]
 
     def toDict(self):
